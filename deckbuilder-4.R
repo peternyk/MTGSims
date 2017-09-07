035579fa5b291ee6451097a2e7b746e6934b8cea
peternyk <nykpete@hotmail.com>
2017-09-07 13:04:45 -0700

    Situational Simulation added
    Added simulation attempting winning in 4 turns, which is the fastest possible win.  Said simulation runs 1000 times and records results for use in linear regression. Also, since it is technically possible to win with 59 lands, I added that as a possibility for the original simulation.

--- deckbuilder-4.R
+++ deckbuilder-4.R
@@ -48,50 +48,48 @@ playTurn = function(deck, hand, battlefield) {
   return (list(deck, hand, battlefield))
 }
 
-landCount = sample(1:59, 1)
-lands = data.frame(type = "land", cost = integer(landCount))
-deck = lands
-while (nrow(deck) < 60) {
-  tempCost = sample(1:landCount, 1)
-  tempCard = data.frame(type = "creature", cost = tempCost)
-  deck = rbind(deck, tempCard)
-}
-deck = deck[sample(1:60),]
-hand = deck[1:6,]
-deck = deck[7:60,]
-battlefield = data.frame(matrix(nrow = 0, ncol = 2))
-colnames(battlefield) = c("type", "cost")
 
-opponentsLife = 20
-lastTurn = FALSE
-turnsPast = 0
-while (opponentsLife > 0)  {
-  if (lastTurn == TRUE) {
-    print(paste("Opponent's Life: ", opponentsLife))
-    print(paste("Cards in play: ", nrow(battlefield)))
-    print(paste("Cards in deck: ", nrow(deck)))
-    print(paste("cards in hand: ", nrow(hand)))
-    print ("You lost!")
-    break
+results = data.frame(matrix(nrow = 0, ncol = 5))
+colnames(results) = c("win", "cost", "cost2", "cost3", "cost4")
+
+for (j in 1:1000) {
+  landCount = sample(1:56, 1)
+  lands = data.frame(type = "land", cost = integer(landCount))
+  deck = lands
+  deck = rbind(deck, data.frame(type="creature", cost=1))
+  while (nrow(deck) < 60) {
+    tempCost = sample(1:4, 1)
+    tempCard = data.frame(type = "creature", cost = tempCost)
+    deck = rbind(deck, tempCard)
   }
-  turnsPast = turnsPast + 1
-  hand = rbind(hand, deck[1,])
-  if (nrow(deck) == 1) {
-    lastTurn = TRUE
-    deck = data.frame(matrix(nrow = 0, ncol = 2))
-    colnames(deck) = c("type", "cost")
-  } else {
+  tempCost1 = nrow(deck[deck$cost == 1,])
+  tempCost2 = nrow(deck[deck$cost == 2,])
+  tempCost3 = nrow(deck[deck$cost == 3,])
+  tempCost4 = nrow(deck[deck$cost == 4,])
+  deck = deck[sample(1:60),]
+  hand = deck[1:6,]
+  deck = deck[7:60,]
+  battlefield = data.frame(matrix(nrow = 0, ncol = 2))
+  colnames(battlefield) = c("type", "cost")
+  
+  opponentsLife = 20
+  for (i in 1:5)  {
+    if (i == 5) {
+      results = rbind(results, data.frame(win = 0, cost1 = tempCost1, cost2 = tempCost2, cost3 = tempCost3, cost4 = tempCost4))
+      break
+    }
+    hand = rbind(hand, deck[1,])
     deck = deck[2:nrow(deck),]
+    newBoardState = playTurn(deck, hand, battlefield)
+    deck = newBoardState[[1]]
+    hand = newBoardState[[2]]
+    battlefield = newBoardState[[3]]
+    opponentsLife = opponentsLife - sum(battlefield[battlefield$type == "creature", "cost"])
+    if (opponentsLife <= 0) {
+      results = rbind(results, data.frame(win = 1, cost1 = tempCost1, cost2 = tempCost2, cost3 = tempCost3, cost4 = tempCost4))
+      break
+    }
   }
-  newBoardState = playTurn(deck, hand, battlefield)
-  deck = newBoardState[[1]]
-  hand = newBoardState[[2]]
-  battlefield = newBoardState[[3]]
-  opponentsLife = opponentsLife - sum(battlefield[battlefield$type == "creature", "cost"])
-  print(paste("Opponent's Life: ", opponentsLife))
-  print(paste("Cards in play: ", nrow(battlefield)))
-  print(paste("Cards in deck: ", nrow(deck)))
-  print(paste("cards in hand: ", nrow(hand)))
-  if (opponentsLife <= 0)
-    print(paste("You won in ", turnsPast))
+  print(paste("Win Count: ", nrow(results[results$win == 1,])))
+  print(paste("Loss Count: ", nrow(results[results$win == 0,])))
 }
